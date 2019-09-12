4b297a9c1fe193fc5467f0a1f758744e8a83d714
aadalid5 <arieladalid07@gmail.com>
2019-09-12 14:18:31 -0400

    nothing special

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -4,16 +4,19 @@ import Card from './Card';
 class Cardlist extends React.Component{
     constructor(props){
         super(props);
+        //*********************************************************************//
+        //(1)asign individually,   ---- //
         //console.log(props);
-
-        /* this.name = this.props.robots[0].name;
+        /* 
+        this.name = this.props.robots[0].name;
         this.email = this.props.robots[0].email;
         this.id = this.props.robots[0].id; */
 
-
+        //*********************************************************************//
+        //(2) another manner of asignation ----//
         const {robots, color} = this.props; 
-        this.robot = robots;
-        this.color = color;
+        this.Urobot = robots;
+        this.Color = color;  // just and arbitrary prop for learning purposes
         //console.log(this.robot);
         //console.log(this.color);
         
@@ -22,13 +25,17 @@ class Cardlist extends React.Component{
     render(){
         return (
             <div>
-                <Card name={this.robot[0].name} email={this.robot[0].email} id={this.robot[0].id}/>
+                {/* without using the constructor ---> */ }
+                {/* <Card name={this.props.robots[0].name} email={this.props.robots[0].email} id={this.props.robots[0].id}/> */}
+                
+                {/*(1) other method, using the constructor, asign individualy */}
                 {/* <Card name={this.name} email={this.email} id={this.id}/> */}
-                {/* <Card name={this.props.robots[0].name} email={this.props.robots[0].email} id={this.props.robots[0].id}/>
-                <Card name={this.props.robots[1].name} email={this.props.robots[1].email} id={this.props.robots[1].id}/>
-                <Card name={this.props.robots[2].name} email={this.props.robots[2].email} id={this.props.robots[2].id}/> */}
-                
-                
+
+                {/*(2) other method, using the constructor  */}
+                <Card name={this.Urobot[0].name} email={this.Urobot[0].email} id={this.Urobot[0].id}/>
+                <Card name={this.Urobot[1].name} email={this.Urobot[1].email} id={this.Urobot[1].id}/>
+                <Card name={this.Urobot[2].name} email={this.Urobot[2].email} id={this.Urobot[2].id}/>
+                                
             </div> 
         );
     }
