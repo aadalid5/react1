d2682adee239396c21eb9415a312ef95b09bbb8b
aadalid5 <arieladalid07@gmail.com>
2019-09-12 10:07:08 -0400

    first react commit

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -3,16 +3,20 @@ import Card from './Card';
 
 class Cardlist extends React.Component{
     constructor(props){
-        super()
+        super(props);
+        this.name = this.props.robots[0].name;
+        this.email = this.props.robots[0].email;
+        this.id = this.props.robots[0].id;
         
     }
 
     render(){
         return (
             <div>
-                <Card name={this.props.robots[0].name} email={this.props.robots[0].email} id={this.props.robots[0].id}/>
+                <Card name={this.name} email={this.email} id={this.id}/>
+                {/* <Card name={this.props.robots[0].name} email={this.props.robots[0].email} id={this.props.robots[0].id}/>
                 <Card name={this.props.robots[1].name} email={this.props.robots[1].email} id={this.props.robots[1].id}/>
-                <Card name={this.props.robots[2].name} email={this.props.robots[2].email} id={this.props.robots[2].id}/>
+                <Card name={this.props.robots[2].name} email={this.props.robots[2].email} id={this.props.robots[2].id}/> */}
                 
                 
             </div> 
